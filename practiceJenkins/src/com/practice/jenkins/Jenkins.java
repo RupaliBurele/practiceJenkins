712318195f3e66106f6d81e6b53d7dc9c33310c9
Rupali <Rupali@192.168.1.11>
2020-08-15 20:06:25 +0530

    commited new practice project

---
+++ practiceJenkins/src/com/practice/jenkins/Jenkins.java
@@ -0,0 +1,17 @@
+package com.practice.jenkins;
+
+public class Jenkins {
+	public static void main(String[] args) {
+		
+		System.out.println("Calculator Started");
+		
+		int a=100,b=100;
+		int sum=a+b;
+		System.out.println("sum:"+sum);
+		
+		System.out.println("Calculator Stop");
+
+	}
+
+
+}
